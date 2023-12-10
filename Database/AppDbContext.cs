06f710035e0db310c3df4843f4e972aa81b9f728
Oreazuz <Omaddams@hotmail.com>
2023-12-10 19:49:04 +0100

    some more seed data and plantdetail window ui fix

--- Database/AppDbContext.cs
+++ Database/AppDbContext.cs
@@ -36,9 +36,17 @@ namespace GreenThumbProject.Database
 
             modelBuilder.Entity<UserModel>().HasData(new UserModel { UserId = 1, UserName = "admin", UserPassword = "admin", UserEmail = "admin@admin.admin" });
 
-            modelBuilder.Entity<PlantModel>().HasData(new PlantModel { PlantId = 3, PlantName = "Lotus", PlantDescription = "A beautiful aquatic plant, considered holy in some cultures" }, new PlantModel { PlantId = 4, PlantName = "Lily", PlantDescription = "Lilium is a genus of herbaceous flowering plants growing from bulbs, all with large prominent flowers. " });
+            modelBuilder.Entity<PlantModel>().HasData(new PlantModel { PlantId = 3, PlantName = "Lotus", PlantDescription = "A beautiful aquatic plant, considered holy in some cultures" },
+                new PlantModel { PlantId = 4, PlantName = "Lily", PlantDescription = "Lilium is a genus of herbaceous flowering plants growing from bulbs, all with large prominent flowers. " },
+                new PlantModel { PlantId = 5, PlantName = "Azalea", PlantDescription = "Azaleas bloom in the spring, their flowers often lasting several weeks." },
+                new PlantModel { PlantId = 6, PlantName = "Chrysanthemum", PlantDescription = "Most species originate from East Asia and the center of diversity is in China. Countless horticultural varieties and cultivars exist." },
+                new PlantModel { PlantId = 7, PlantName = "Daffodil", PlantDescription = "Narcissus is a genus of predominantly spring flowering perennial plants of the amaryllis family, Amaryllidaceae. Various common names including daffodil, narcissus and jonquil, are used to describe all or some members of the genus." }
+                );
 
-            modelBuilder.Entity<InstructionModel>().HasData(new InstructionModel { InstructionId = 1, InstructionDescription = "Keep it in your pond", PlantId = 3 });
+            modelBuilder.Entity<InstructionModel>().HasData(new InstructionModel { InstructionId = 1, InstructionDescription = "Keep it in your pond", PlantId = 3 },
+                new InstructionModel { InstructionId = 2, InstructionDescription = "Water", PlantId = 4 },
+                new InstructionModel { InstructionId = 3, InstructionDescription = "Keep lightly shaded", PlantId = 4 },
+                new InstructionModel { InstructionId = 4, InstructionDescription = "Fertalize when planting", PlantId = 5 });
         }
     }
 }
---
+++ Migrations/20231210002316_moreSeeds.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GreenThumbProject.Migrations
+{
+    public partial class moreSeeds : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Plants",
+                columns: new[] { "PlantId", "PlantDescription", "PlantName" },
+                values: new object[,]
+                {
+                    { 5, "Azaleas bloom in the spring, their flowers often lasting several weeks.", "Azalea" },
+                    { 6, "Most species originate from East Asia and the center of diversity is in China. Countless horticultural varieties and cultivars exist.", "Chrysanthemum" },
+                    { 7, "Narcissus is a genus of predominantly spring flowering perennial plants of the amaryllis family, Amaryllidaceae. Various common names including daffodil, narcissus and jonquil, are used to describe all or some members of the genus.", "Daffodil" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "UserPassword",
+                value: "4tWbx5vN7mIYljZjsOsGhA==");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Plants",
+                keyColumn: "PlantId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Plants",
+                keyColumn: "PlantId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Plants",
+                keyColumn: "PlantId",
+                keyValue: 7);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                column: "UserPassword",
+                value: "4tWbx5vN7mIYljZjsOsGhA==");
+        }
+    }
+}
